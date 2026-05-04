0795af78343722469cf4003a8b8060002bf4909f
ShubHH03 <mak.shubham2003@gmail.com>
2026-05-04 17:24:50 +0530

    fetch & dsiplay image

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+	const [image, setImage] = useState();
 
+	const fetchData = async () => {
+		try {
+			const response = await fetch(
+				'https://api.freeapi.app/api/v1/public/cats/cat/random',
+			);
+			const data = await response.json();
+			const catImage = data.data.image;
+			setImage(catImage);
+		} catch (error) {
+			console.log(`Error fetching data`, error);
+		}
+	};
 
-
-
-  
+	useEffect(() => {
+		fetchData();
+	}, []);
 
 	return (
 		<>
-    <h1>Random Cat Viewer</h1>
+			<h1>Random Cat Viewer</h1>
+			<img src={image} alt="cat" height="400px" width="400px" />
+			<button onClick={fetchData}>New Cat</button>
 		</>
 	);
 }
